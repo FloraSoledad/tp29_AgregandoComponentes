1e8484a8465dc20bee87e0da98b0acc96e5b7e4d
FloraSoledad <vargas.flora@hotmail.com>
2023-01-03 08:25:21 -0300

    trabajando componentes terminado

---
+++ src/components/table/Row.js
@@ -0,0 +1,24 @@
+
+import React from 'react'
+
+const Row = (props) => {
+    return (
+        <>
+            <tr>
+                <td>{props.titulo}</td>
+                <td>{props.duracion}</td>
+                <td>{props.rating}</td>
+                <td>
+                    <ul>
+                        {
+                            props.genero.map((item, i) => <li key= {item + i}>{item}</li>)
+                        }
+                    </ul>
+                </td>
+                <td>{props.premios}</td>
+            </tr>
+        </>
+    );
+}
+
+export default Row;
